0a7248e653633a45faad5352c9561f4c9609b907
zhibin <zhibin@guanghe.tv>
2021-04-15 20:05:34 +0800

    修改nacos连接客户端

--- setup/nacos/NacosRemoteConfig.go
+++ setup/nacos/NacosRemoteConfig.go
@@ -3,122 +3,61 @@ package nacos
 import (
 	"bytes"
 	"errors"
+	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/nacos-group/nacos-sdk-go/clients"
 	"io"
 	"os"
-	"path/filepath"
+	"strings"
 	"sync"
 
-	"fmt"
-	"github.com/magiconair/properties"
-	"github.com/shima-park/agollo"
 	"github.com/spf13/viper"
 	crypt "github.com/xordataexchange/crypt/config"
 )
 
+
 var (
 	ErrUnsupportedProvider = errors.New("This configuration manager is not supported")
 
-	_ viperConfigManager = apolloConfigManager{}
+	_ viperConfigManager = nacosConfigManager{}
 	// getConfigManager方法每次返回新对象导致缓存无效，
 	// 这里通过endpoint作为key复一个对象
 	// key: endpoint+appid value: agollo.Agollo
 	agolloMap sync.Map
 )
 
+
 var (
-	// apollod的appid
-	appID string
-	// 默认为properties，apollo默认配置文件格式
-	defaultConfigType = "properties"
-	// 默认创建Agollo的Option
-	defaultAgolloOptions = []agollo.Option{
-		agollo.AutoFetchOnCacheMiss(),
-		agollo.FailTolerantOnBackupExists(),
+	dataId string
+	group string
+	nameSpaceId string
+	defaultConfigType = "yaml"
+	defaultNacosOptions = vo.NacosClientParam{
+
 	}
+
 )
 
-func SetAppID(appid string) {
-	appID = appid
+type nacosConfigManager struct {
+	nConfigClient config_client.IConfigClient
 }
 
-func SetConfigType(ct string) {
-	defaultConfigType = ct
-}
-
-func SetAgolloOptions(opts ...agollo.Option) {
-	defaultAgolloOptions = opts
-}
-
-type viperConfigManager interface {
-	Get(key string) ([]byte, error)
-	Watch(key string, stop chan bool) <-chan *viper.RemoteResponse
-}
-
-type apolloConfigManager struct {
-	agollo agollo.Agollo
-}
-
-func newApolloConfigManager(appid, endpoint string, opts []agollo.Option) (*apolloConfigManager, error) {
-	if appid == "" {
-		return nil, errors.New("The appid is not set")
-	}
-
-	ag, err := newAgollo(appid, endpoint, opts)
+func (n nacosConfigManager) Get( params vo.ConfigParam ) ([]byte, error) {
+	content, err := n.nConfigClient.GetConfig(params)
 	if err != nil {
 		return nil, err
 	}
-
-	return &apolloConfigManager{
-		agollo: ag,
-	}, nil
-
+	return []byte(content), nil
 }
 
-func newAgollo(appid, endpoint string, opts []agollo.Option) (agollo.Agollo, error) {
-	i, found := agolloMap.Load(endpoint + "/" + appid)
-	if !found {
-		ag, err := agollo.New(
-			endpoint,
-			appid,
-			opts...,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		// 监听并同步apollo配置
-		ag.Start()
-
-		agolloMap.Store(endpoint + "/" + appid, ag)
-
-		return ag, nil
-	}
-	return i.(agollo.Agollo), nil
-}
-
-func (cm apolloConfigManager) Get(namespace string) ([]byte, error) {
-	configs := cm.agollo.GetNameSpace(namespace)
-	return marshalConfigs(getConfigType(namespace), configs)
-}
-
-func marshalConfigs(configType string, configs map[string]interface{}) ([]byte, error) {
-	var bts []byte
-	var err error
-	switch configType {
-	case "json", "yml", "yaml", "xml":
-		content := configs["content"]
-		if content != nil {
-			bts = []byte(content.(string))
-		}
-	case "properties":
-		bts, err = marshalProperties(configs)
-	}
-	return bts, err
-}
-
-func (cm apolloConfigManager) Watch(namespace string, stop chan bool) <-chan *viper.RemoteResponse {
+func (n nacosConfigManager) Watch(params vo.ConfigParam, stop chan bool) <-chan *RemoteResponse {
 	resp := make(chan *viper.RemoteResponse)
-	backendResp := cm.agollo.WatchNamespace(namespace, stop)
+
+
+	params.OnChange = func(namespace, group, dataId, data string) {
+
+	}
+
 	go func() {
 		for {
 			select {
@@ -141,6 +80,34 @@ func (cm apolloConfigManager) Watch(namespace string, stop chan bool) <-chan *vi
 		}
 	}()
 	return resp
+
+
+
+
+
+
+
+	err :=  n.nConfigClient.ListenConfig(vo.ConfigParam{
+		DataId: dataId,
+		Group:  group,
+		OnChange: func(namespace, group, dataId, data string) {
+			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", content:" + data)
+			err = NacosConfig.ReadConfig(strings.NewReader(data))
+			Logger.Logger.Info(NacosConfig)
+			if err != nil {
+				fmt.Println("Viper解析配置失败:", err)
+				panic(err)
+				return
+			}
+		},
+	})
+
+}
+
+
+type viperConfigManager interface {
+	Get(key string) ([]byte, error)
+	Watch(key string, stop chan bool) <-chan *viper.RemoteResponse
 }
 
 type configProvider struct {
@@ -226,6 +193,32 @@ func (rc configProvider) WatchChannel(rp viper.RemoteProvider) (<-chan *viper.Re
 	}
 }
 
+func newNacosConfigManager(nacosClientParam  vo.NacosClientParam)(*nacosConfigManager, error)  {
+	if dataId == "" {
+		return nil, errors.New("The dataId is not set")
+	}
+
+	if group == "" {
+		return nil, errors.New("The group is not set")
+	}
+
+	if nameSpaceId == "" {
+		return nil, errors.New("The nameSpaceId is not set")
+	}
+
+	client, err := clients.NewConfigClient(
+		nacosClientParam,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &nacosConfigManager{
+		nConfigClient: client,
+	}, nil
+}
+
 func getConfigManager(rp viper.RemoteProvider) (interface{}, error) {
 	if rp.SecretKeyring() != "" {
 		kr, err := os.Open(rp.SecretKeyring())
@@ -239,8 +232,8 @@ func getConfigManager(rp viper.RemoteProvider) (interface{}, error) {
 			return crypt.NewEtcdConfigManager([]string{rp.Endpoint()}, kr)
 		case "consul":
 			return crypt.NewConsulConfigManager([]string{rp.Endpoint()}, kr)
-		case "apollo":
-			return nil, errors.New("The Apollo configuration manager is not encrypted")
+		case "nacos":
+			return nil, errors.New("The Nacos configuration manager is not support encrypted")
 		default:
 			return nil, ErrUnsupportedProvider
 		}
@@ -250,53 +243,18 @@ func getConfigManager(rp viper.RemoteProvider) (interface{}, error) {
 			return crypt.NewStandardEtcdConfigManager([]string{rp.Endpoint()})
 		case "consul":
 			return crypt.NewStandardConsulConfigManager([]string{rp.Endpoint()})
-		case "apollo":
-			return newApolloConfigManager(appID, rp.Endpoint(), defaultAgolloOptions)
+		case "nacos":
+			return newNacosConfigManager(defaultNacosOptions)
 		default:
 			return nil, ErrUnsupportedProvider
 		}
 	}
 }
 
-// 配置文件有多种格式，例如：properties、xml、yml、yaml、json等。同样Namespace也具有这些格式。在Portal UI中可以看到“application”的Namespace上有一个“properties”标签，表明“application”是properties格式的。
-// 如果使用Http接口直接调用时，对应的namespace参数需要传入namespace的名字加上后缀名，如datasources.json。
-func getConfigType(namespace string) string {
-	ext := filepath.Ext(namespace)
-
-	if len(ext) > 1 {
-		fileExt := ext[1:]
-		// 还是要判断一下碰到，TEST.Namespace1
-		// 会把Namespace1作为文件扩展名
-		for _, e := range viper.SupportedExts {
-			if e == fileExt {
-				return fileExt
-			}
-		}
-	}
-
-	return defaultConfigType
-}
-
 func init() {
 	viper.SupportedRemoteProviders = append(
 		viper.SupportedRemoteProviders,
-		"apollo",
+		"nacos",
 	)
 	viper.RemoteConfig = &configProvider{}
-}
-
-func marshalProperties(c map[string]interface{}) ([]byte, error) {
-	p := properties.NewProperties()
-	for key, val := range c {
-		_, _, err := p.Set(key, fmt.Sprint(val))
-		if err != nil {
-			return nil, err
-		}
-	}
-	buff := bytes.NewBuffer(nil)
-	_, err := p.WriteComment(buff, "#", properties.UTF8)
-	if err != nil {
-		return nil, err
-	}
-	return buff.Bytes(), nil
 }
